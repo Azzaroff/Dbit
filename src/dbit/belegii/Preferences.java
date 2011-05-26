ba87974977ed3bb2c4d00a2234b637b239e18f3c
Paul <paul.kramer@uni-weimar.de>
2011-05-27 01:24:02 +0200

    Preferences Menu eingefügt
    Change-Id: I210f1b6d1c2c51496f0717784913a15ffe11536e

---
+++ src/dbit/belegii/Preferences.java
@@ -0,0 +1,13 @@
+package dbit.belegii;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
--- src/dbit/belegii/cacheActivity.java
+++ src/dbit/belegii/cacheActivity.java
@@ -8,8 +8,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,6 +21,9 @@ import android.widget.Toast;
 
 public class cacheActivity extends Activity {
     private Buffer buffer = new LRU(3);
+    
+    //preferences
+    SharedPreferences preferences;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -24,9 +31,31 @@ public class cacheActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-    
-
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //generates a new buffer
+        switch(Integer.parseInt(preferences.getString("bufferType", "0"))){
+	        case 0:{
+	        	buffer = new FiFo(Integer.parseInt(preferences.getString("buffersize", "3")));
+	        	break;
+	        }
+	        case 1:{
+	        	buffer = new LRU(Integer.parseInt(preferences.getString("buffersize", "3")));
+	        	break;
+	        }
+	        case 2:{
+	        	buffer = new FiFo(Integer.parseInt(preferences.getString("buffersize", "3")));
+	        	break;
+	        }
+        }
     }
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode == KeyEvent.KEYCODE_MENU){
+			startActivity(new Intent(this, Preferences.class));
+		}
+	    return(true);
+	}
     
     public void startQuery(View view) {
     	final EditText query = (EditText)findViewById(R.id.edit);
@@ -77,5 +106,28 @@ public class cacheActivity extends Activity {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+    }
+    
+    @Override
+    public void onResume(){
+    	super.onResume();
+    	//generates a new buffer
+        switch(Integer.parseInt(preferences.getString("bufferType", "0"))){
+	        case 0:{
+	        	buffer = new FiFo(Integer.parseInt(preferences.getString("buffersize", "3")));
+	        	Log.i(this.getClass().getSimpleName(), "new buffer type: FIFO");
+	        	break;
+	        }
+	        case 1:{
+	        	buffer = new LRU(Integer.parseInt(preferences.getString("buffersize", "3")));
+	        	Log.i(this.getClass().getSimpleName(), "new buffer type: LRU");
+	        	break;
+	        }
+	        case 2:{
+	        	buffer = new FiFo(Integer.parseInt(preferences.getString("buffersize", "3")));
+	        	Log.i(this.getClass().getSimpleName(), "new buffer type: FIFO");
+	        	break;
+	        }
+        }
     }
 }
