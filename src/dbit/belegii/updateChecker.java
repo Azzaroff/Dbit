dc761a827b7044ad6999fd839dd554161624a63b
Azzaroff <azza@roff>
2011-06-09 13:56:49 +0200

    skelleton regex functions

---
+++ src/dbit/belegii/updateChecker.java
@@ -0,0 +1,16 @@
+package dbit.belegii;
+
+public class updateChecker {
+	
+	//returns tables that need to be removed from the buffer as String e.g. String("a,b,c")
+	//return null if no match was found
+	public String checkUpdate(String sql){
+		return null;
+	}
+	
+	//return true if table was found in sql query
+	//otherwise returns false
+	public boolean checkTable(String table,String sql){
+		return false;
+	}
+}
